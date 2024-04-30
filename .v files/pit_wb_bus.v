01387f3e0de63d02e79899b482c0e2456bb628b5
3b3al-MV <153736149+3b3al-MV@users.noreply.github.com>
2024-04-30 13:37:15 +0300

    Update and rename pit_wb_bus.v to .v files/pit_wb_bus.v

--- .v files/pit_wb_bus.v
+++ .v files/pit_wb_bus.v
@@ -1,41 +1,3 @@
-////////////////////////////////////////////////////////////////////////////////
-//
-//  WISHBONE revB.2 compliant Programable Interrupt Timer - Bus interface
-//
-//  Author: Ahmed Abdelazeem
-//          ahmed-abdelazeem@outlook.com
-//
-//  
-//
-////////////////////////////////////////////////////////////////////////////////
-// Copyright (c) 2020, Ahmed Abdelazeem
-//
-// All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-//     * Redistributions of source code must retain the above copyright
-//       notice, this list of conditions and the following disclaimer.
-//     * Redistributions in binary form must reproduce the above copyright
-//       notice, this list of conditions and the following disclaimer in the
-//       documentation and/or other materials provided with the distribution.
-//     * Neither the name of the <organization> nor the
-//       names of its contributors may be used to endorse or promote products
-//       derived from this software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY Ahmed Abdelazeem ''AS IS'' AND ANY
-// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-// DISCLAIMED. IN NO EVENT SHALL Ahmed Abdelazeem BE LIABLE FOR ANY
-// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-////////////////////////////////////////////////////////////////////////////////
-// 45678901234567890123456789012345678901234567890123456789012345678901234567890
-
 module pit_wb_bus #(parameter ARST_LVL = 1'b0,    // asynchronous reset level
   		    parameter DWIDTH = 16,
                     parameter SINGLE_CYCLE = 1'b0)
@@ -67,7 +29,7 @@ module pit_wb_bus #(parameter ARST_LVL = 1'b0,    // asynchronous reset level
 
   // Wires
   wire       eight_bit_bus;
-  wire       module_sel;      // This module is selected for bus transaction
+  wire       module_sel;      // This module is selected for a bus transaction
   wire       wb_wacc;         // WISHBONE Write Strobe
   wire       wb_racc;         // WISHBONE Read Access (Clock gating signal)
   wire [2:0] address;         // Select either direct or latched address
@@ -89,8 +51,8 @@ module pit_wb_bus #(parameter ARST_LVL = 1'b0,    // asynchronous reset level
   assign wb_ack_o   = SINGLE_CYCLE ? module_sel : (bus_wait_state && module_sel);
   assign address    = SINGLE_CYCLE ? wb_adr_i : addr_latch;
 
-  // generate acknowledge output signal, By using register all accesses takes two cycles.
-  //  Accesses in back to back clock cycles are not possable.
+  // generate acknowledge output signal, By using register all accesses take two cycles.
+  //  Accesses in back-to-back clock cycles are not possible.
   always @(posedge wb_clk_i or negedge async_rst_b)
     if (!async_rst_b)
       bus_wait_state <=  1'b0;
