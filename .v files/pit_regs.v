685d7e1eda104b626272c7107cf8098d68dc2231
3b3al-MV <153736149+3b3al-MV@users.noreply.github.com>
2024-04-30 13:36:11 +0300

    Update and rename pit_regs.v to .v files/pit_regs.v

--- .v files/pit_regs.v
+++ .v files/pit_regs.v
@@ -1,41 +1,3 @@
-////////////////////////////////////////////////////////////////////////////////
-//
-//  WISHBONE revB.2 compliant Programable Interrupt Timer - Control registers
-//
-//  Author: Ahmed Abdelazeem
-//          ahmed-abdelazeem@outlook.com
-//
-//  
-//
-////////////////////////////////////////////////////////////////////////////////
-// Copyright (c) 2020, Ahmed Abdelazeem
-//
-// All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-//     * Redistributions of source code must retain the above copyright
-//       notice, this list of conditions and the following disclaimer.
-//     * Redistributions in binary form must reproduce the above copyright
-//       notice, this list of conditions and the following disclaimer in the
-//       documentation and/or other materials provided with the distribution.
-//     * Neither the name of the <organization> nor the
-//       names of its contributors may be used to endorse or promote products
-//       derived from this software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY Ahmed Abdelazeem ''AS IS'' AND ANY
-// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-// DISCLAIMED. IN NO EVENT SHALL Ahmed Abdelazeem BE LIABLE FOR ANY
-// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-////////////////////////////////////////////////////////////////////////////////
-// 45678901234567890123456789012345678901234567890123456789012345678901234567890
-
 module pit_regs #(parameter ARST_LVL = 1'b0,  // asynchronous reset level
                   parameter COUNT_SIZE = 16,
                   parameter NO_PRESCALE = 1'b0,
@@ -60,11 +22,11 @@ module pit_regs #(parameter ARST_LVL = 1'b0,  // asynchronous reset level
 
   // registers
   reg  [ 3:0] pit_pre;    // Optional register for PIT Prescale Counter modulo
-                          //  This register should be removed durning synthesis
+                          //  This register should be removed during synthesis
 			  //  if the "NO_PRESCALE" parameter is set
 
   // Wires
-  wire [15:0] write_data; // Data bus mux for 8 or 16 bit module bus
+	wire [15:0] write_data; // Data bus mux for 8 or 16-bit module bus
 
   //
   // module body
