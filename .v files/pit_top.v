4e033c87c8d2146d7723cd42459defb33402d7fb
3b3al-MV <153736149+3b3al-MV@users.noreply.github.com>
2024-04-30 13:36:44 +0300

    Update pit_top.v

--- .v files/pit_top.v
+++ .v files/pit_top.v
@@ -1,40 +1,3 @@
-////////////////////////////////////////////////////////////////////////////////
-//
-//  WISHBONE revB.2 compliant Programable Interrupt Timer - Top-level
-//
-//  Author: Ahmed Abdelazeem
-//          ahmed-abdelazeem@outlook.com
-//
-//  
-//
-////////////////////////////////////////////////////////////////////////////////
-// Copyright (c) 2020, Ahmed Abdelazeem
-//
-// All rights reserved.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-//     * Redistributions of source code must retain the above copyright
-//       notice, this list of conditions and the following disclaimer.
-//     * Redistributions in binary form must reproduce the above copyright
-//       notice, this list of conditions and the following disclaimer in the
-//       documentation and/or other materials provided with the distribution.
-//     * Neither the name of the <organization> nor the
-//       names of its contributors may be used to endorse or promote products
-//       derived from this software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY Ahmed Abdelazeem ''AS IS'' AND ANY
-// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-// DISCLAIMED. IN NO EVENT SHALL Ahmed Abdelazeem BE LIABLE FOR ANY
-// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-////////////////////////////////////////////////////////////////////////////////
-// 45678901234567890123456789012345678901234567890123456789012345678901234567890
 `include "pit_count.v"
 `include "pit_prescale.v"
 `include "pit_regs.v"
@@ -70,8 +33,8 @@ module pit_top #(parameter ARST_LVL = 1'b0,      // asynchronous reset level
   
   wire [COUNT_SIZE-1:0] mod_value;     // Main Counter Modulo
   wire [COUNT_SIZE-1:0] cnt_n;         // PIT Counter Value
-  wire                  async_rst_b;   // Asyncronous reset
-  wire                  sync_reset;    // Syncronous reset
+  wire                  async_rst_b;   // Asynchronous reset
+  wire                  sync_reset;    // Synchronous reset
   wire           [ 3:0] write_regs;    // Control register write strobes
   wire                  prescale_out;  //
   wire                  pit_flg_clr;   // Clear PIT Rollover Status Bit
